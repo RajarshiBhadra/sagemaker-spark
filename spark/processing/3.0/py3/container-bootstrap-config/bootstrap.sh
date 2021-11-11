752125dc1b75f0112e7b263373a4d81c3144d456
Rajarshi Bhadra <bhadrarajarshi9@gmail.com>
2021-11-11 23:07:44 +0530

    added packages

--- spark/processing/3.0/py3/container-bootstrap-config/bootstrap.sh
+++ spark/processing/3.0/py3/container-bootstrap-config/bootstrap.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 set -x
 
-python3 -m pip install pandas openpyxl==3.0.0 nltk NRCLex vaderSentiment
+python3 -m pip install pandas openpyxl==3.0.0 nltk NRCLex vaderSentiment bertopic==0.9.3 stanford_openie spacy
 python3 -m nltk.downloader popular
