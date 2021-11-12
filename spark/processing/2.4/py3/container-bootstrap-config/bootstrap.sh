07c9e53d4dd4f187921bb993039f6b5bfc53afd1
Rajarshi Bhadra <bhadrarajarshi9@gmail.com>
2021-11-12 12:54:58 +0530

    Updating spark 2.4

--- spark/processing/2.4/py3/container-bootstrap-config/bootstrap.sh
+++ spark/processing/2.4/py3/container-bootstrap-config/bootstrap.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 set -x
 
-sudo python3 -m pip install pandas, openpyxl==3.0.0,nltk,NRCLex,vaderSentiment
+python3 -m pip install pandas openpyxl==3.0.0 nltk NRCLex vaderSentiment bertopic==0.9.3 stanford_openie spacy wordninja emoji s3fs fsspec spark-nlp==3.1.2
+python3 -m nltk.downloader popular
