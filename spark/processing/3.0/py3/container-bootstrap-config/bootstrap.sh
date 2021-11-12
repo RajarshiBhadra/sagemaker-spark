1a7a419486849b3dc6fb78d2113b0a8c773e0b81
Rajarshi Bhadra <bhadrarajarshi9@gmail.com>
2021-11-12 12:54:21 +0530

    downgrading to spark 2.4
    remove pip install

--- spark/processing/3.0/py3/container-bootstrap-config/bootstrap.sh
+++ spark/processing/3.0/py3/container-bootstrap-config/bootstrap.sh
@@ -1,5 +1,2 @@
-#!/bin/bash
-set -x
+echo "Not implemented"
 
-python3 -m pip install pandas openpyxl==3.0.0 nltk NRCLex vaderSentiment bertopic==0.9.3 stanford_openie spacy wordninja emoji s3fs fsspec spark-nlp==3.1.2
-python3 -m nltk.downloader popular
