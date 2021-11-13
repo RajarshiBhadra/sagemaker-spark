dd8e6bc6e7f53a15ac29c4b5502193853b1c9ce7
Rajarshi Bhadra <bhadrarajarshi9@gmail.com>
2021-11-14 01:14:26 +0530

    Update bootstrap.sh

--- spark/processing/3.0/py3/container-bootstrap-config/bootstrap.sh
+++ spark/processing/3.0/py3/container-bootstrap-config/bootstrap.sh
@@ -6,6 +6,5 @@ python3 -m pip install bertopic stanford_openie
 python3 -m pip install pandas openpyxl==3.0.0 nltk NRCLex vaderSentiment 
 python3 -m pip install spacy wordninja emoji s3fs fsspec spark-nlp==3.3.2 
 python3 -m nltk.downloader popular
-python3 -m pip uninstall numba
-python3 -m pip install numba
+python3 -m pip install numpy==1.20.0
 
