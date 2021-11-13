578aa5eaef0f7d79f628a593516c0856cb8d3f7c
Rajarshi Bhadra <bhadrarajarshi9@gmail.com>
2021-11-14 00:38:28 +0530

    Update hello_py_spark_app.py

--- test/resources/code/python/hello_py_spark/hello_py_spark_app.py
+++ test/resources/code/python/hello_py_spark/hello_py_spark_app.py
@@ -58,4 +58,4 @@ if __name__ == "__main__":
     salesDF.select("date", "sale", double_udf_int("sale").alias("sale_double")).write.json(outputPath)
     print("Saved data")
     
-    import bertopic
+    import stanford_openie
