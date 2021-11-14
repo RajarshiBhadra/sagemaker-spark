680502ed30da9e8bd5eae60c10a1a214dc953b24
Rajarshi Bhadra <rajarshi.bhadra@accenture.com>
2021-11-14 22:53:00 +0530

    Updating NLTK Version

--- spark/processing/3.0/py3/container-bootstrap-config/bootstrap.sh
+++ spark/processing/3.0/py3/container-bootstrap-config/bootstrap.sh
@@ -3,7 +3,7 @@ set -x
 
 python3 -m pip install psutil
 python3 -m pip install bertopic stanford_openie
-python3 -m pip install pandas openpyxl==3.0.0 nltk NRCLex vaderSentiment 
+python3 -m pip install pandas openpyxl==3.0.0 NRCLex vaderSentiment nltk==3.2.5
 python3 -m pip install spacy wordninja emoji s3fs fsspec spark-nlp==3.3.2 
 python3 -m nltk.downloader popular
 python3 -m nltk.downloader 'punkt'
